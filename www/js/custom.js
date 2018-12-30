86f2c829d12851b1615b065b4500f00ea9bb84ba
fased <fa.sedaghat@gmail.com>
2018-12-30 13:09:51 +0330

    update

--- www/js/custom.js
+++ www/js/custom.js
@@ -199,7 +199,8 @@ function login_and_get_data()
 
 function check_net(show_alert,do_loop)
 {
-    // console.log(networkState);
+     console.log(networkState);
+     alert(networkState);
     if (networkState == Connection.NONE) {
         if(show_alert)
         {
